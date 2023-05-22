444d07e87370070278b53a1b451d929e619c879f
jnccabral <68880430+jnccabral@users.noreply.github.com>
2023-05-22 14:55:59 -0300

    added new read data

--- code/read data.R
+++ code/read data.R
@@ -9,9 +9,9 @@
 # Libraries
 #library(readxl)
 library(bannerCommenter)
-#library(dplyr)
 #library(tidyr)
 library(readr)
+library(dplyr)
 
 # Set WD 
 setwd("C:/Users/juanc/Desktop/Papers/logic1800/")
@@ -21,7 +21,26 @@ df <- read_csv("input/deathsweekly.csv",
                          col_names = FALSE, skip = 9)
 
 # Change colnames
-colnames(df)<-c("place","period","year","both_sex","males","females")
+colnames(df)<-c("place","period","both_sex","males","females")
 
 # Remove empty rows
 df <- df[5:nrow(df), ]
+
+# Replace empty with 0
+df[is.na(df$place)==TRUE, ]$place<-"NA"
+
+# Inserting places
+for (row in 1:nrow(df)) {
+  place <- df[row, "place"]
+  if(place!="NA") {
+    next
+  } else{
+    df[row,"place"]$place<-df[row-1,"place"]$place
+  }
+  
+}
+
+# Change week date for week numbers
+df <- df %>%
+  group_by(place) %>%
+  mutate(week_number = row_number())
